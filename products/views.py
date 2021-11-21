6afa1b4c562a36a7e495c0039e260bbf384226dd
ctoweyburke <ctowey21@yahoo.com>
2021-11-21 01:40:43 +0000

    started  project template

--- products/views.py
+++ products/views.py
@@ -3,11 +3,10 @@ from .models import Product
 
 # Create your views here.
 
-
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.a;;()
+    products = Product.objects.all()
 
     context = {
         'products': products,
