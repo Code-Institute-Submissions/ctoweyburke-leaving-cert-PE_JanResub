510d1cfb40df2d1cc4980c112e7f47ed22213019
ctoweyburke <ctowey21@yahoo.com>
2021-11-23 00:26:13 +0000

    bug detected but not yet solved

--- products/views.py
+++ products/views.py
@@ -2,67 +2,69 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+        """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.all()
-    query = None
-    categories = None
-    sort = None
-    direction = None
+        products = Product.objects.all()
+        query = None
+        categories = None
+        sort = None
+        direction = None
 
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower.name'
-                products = products.annotate(lower_name=('name'))
-            if sortkey == 'category':
-                sortkey = 'category_name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
+        if request.GET:
+            if 'sort' in request.GET:
+                sortkey = request.GET['sort']
+        sort = sortkey
+        if sortkey == 'name':
+                sortkey = 'lower_name'
+        products = products.annotate(lower_name=Lower('name'))
+        if sortkey == 'category':
+            sortkey = 'category__name'
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+        if direction == 'desc':
+            sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')#
-            products = products.filter(category__name__in=categories)
-            categories = Category_objects.filter(name__in=categories)
-            
+            categories = request.GET['category'].split(',')
+        products = products.filter(category__name__in=categories)
+        categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
-            if not query:
-                message.error(request, "You didn't enter any search criteria")
-                return redirect(reverse('products'))
-        
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+        return redirect(reverse('products'))
+
         queries = Q(name__icontains=query) | Q(description__icontains=query)
         products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}'
+        current_sorting = f'{sort}_{direction}'
 
-    context = {
+        context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
-    }
+        }
 
-    return render(request, 'products/products.html', context)
+        return render(request, 'products/products.html', context)
 
 
-def product_detail(request, product_id):
-    """ A view to show individual products details """
+        def product_detail(request, product_id):
+            """ A view to show individual product details """
 
-    product = get_object_or_404(Product, pk=product_id)
+        product = get_object_or_404(Product, pk=product_id)
 
-    context = {
+        context = {
         'product': product,
-    }
-    return render(request, 'products/product_detail.html', context)
+        }
+        
+        return render(request, 'products/product_detail.html', context)
