2a11ff473b4f7602e02849d9d7e9d1afe173cd90
ctoweyburke <ctowey21@yahoo.com>
2021-12-07 13:55:25 +0000

    add product html added

--- leavingcertpe/settings.py
+++ leavingcertpe/settings.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from pathlib import Path
 
 import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
