7c2bdc478d8cfbeb6b7b3a5e5a73b2d619464a50
ctoweyburke <ctowey21@yahoo.com>
2022-01-15 15:10:12 +0000

    removed secret key and set debug

--- leavingcertpe/settings.py
+++ leavingcertpe/settings.py
@@ -24,10 +24,10 @@ if path.exists("env.py"):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+!e+tq2k521e((zw0y07yl&@i^6)%p7))*n^1cb339-ojogu$v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['leavingcertpe.herokuapp.com', 'localhost']
 
