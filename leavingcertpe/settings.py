f828f009a8117830cad853c881c17b3657e6a4ce
ctoweyburke <ctowey21@yahoo.com>
2022-01-15 00:11:55 +0000

    Updated Stripe Keys

--- leavingcertpe/settings.py
+++ leavingcertpe/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 from os import path
 if path.exists("env.py"):
-  import env 
+        import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
